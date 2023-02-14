146b4d2e7d02e17883a653553ec086f22d6a8a06
bruceeifler@gmail.com <bruceeifler@gmail.com>
2023-02-13 18:44:54 -0700

    z

--- myselect.php
+++ myselect.php
@@ -1,4 +1,4 @@
-<html>
+
 
 <?php
 $name = $_GET['name'];
@@ -17,10 +17,23 @@ else
     $res = $db->query("select * from cars where name='$name'");
 }
 
+/*
 while ($row = $res->fetchArray()) {
 echo "{$row['id']} {$row['name']} {$row['price']} \n";
 }
 
+*/
+$row = $res->fetchArray();
+
+/*
+$row['order_id'] = $order_id;
+$row['amount'] = $amount;
+$row['response_code'] = $response_code;
+$row['response_desc'] = $response_desc;
+*/
+
+echo json_encode($row);
+
 
 ?>
-</html>
+
